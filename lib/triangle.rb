e75fb9aeb372941d922da288e775313b57083883
Robert Tate <btate712@gmail.com>
2019-03-16 12:38:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    if side_1 == side_2 || side_2 == side_3 # check if at least 2 sides are equal
+      if side_1 == side_2 && side_2 == side_3 # if all equal => equilateral
+        return_value = :equilateral
+      elsif side_1 != side_2 || side_2 != side_3 # if at least 2 equal one pair is not equal => isosceles
+        return_value = :isosceles
+      end
+    elsif side_1 + side_2 < side_3 || side_2 + side_3 < side_1 || side_3 + side_1 < side_2
+      raise TriangleError
+    else
+      return_value = :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "You've entered dimensions for an imposible triangle!  The sum of the lengths of "
+      "any two sides must be greater than the length of the remaining side."
+    end 
 end
