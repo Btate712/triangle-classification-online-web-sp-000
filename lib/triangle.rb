7e694b3c15ccd46fd4d6bcf4c94bc0a5811b587a
Robert Tate <btate712@gmail.com>
2019-03-16 12:47:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end
 
   def kind
-    if (side_1 + side_2 <= side_3) || (side_2 + side_3 <= side_1) || (side_3 + side_1 <= side_2) || 
+    if (side_1 + side_2 <= side_3) || (side_2 + side_3 <= side_1) || (side_3 + side_1 <= side_2) ||
         side_1 == 0 || side_2 == 0 || side_3 == 0 # if any two sides don't add up to greater than the length of the third side
         raise TriangleError                       # or if any side is equal to zero, raise an error.
     elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3 # check if at least 2 sides are equal
@@ -17,7 +17,7 @@ class Triangle
       elsif side_1 != side_2 || side_2 != side_3 || side_1 != side_3 # if at least 2 equal one pair is not equal => isosceles
         return_value = :isosceles  # if at least 2 equal one pair is not equal => isosceles
           return_value = :isosceles
-      end               
+      end
     else
       return_value = :scalene
     end
